aa0334436049d3581a3219bd2f9101306fdc857c
unknown <mcampbell@osiris06201.osiris.net>
2014-04-16 18:46:33 +0200

    Version 5 Commit

--- generator-geni/app/index.js
+++ generator-geni/app/index.js
@@ -18,13 +18,13 @@ var GeniGenerator = yeoman.generators.Base.extend({
             this.copy("_footer.html", "app/footer.html");
             this.copy("_gruntfile.js", "Gruntfile.js");
             this.copy("_package.json", "package.json");
-            this.copy("_main.css", "app/css/main.css");    
-         
-            var context = { 
-                site_name: this.appName, 
-				casino_name: this.casinoName, 
+            this.copy("_main.css", "app/css/main.css");
+
+            var context = {
+                site_name: this.appName,
+	     casino_name: this.casinoName,
             };
-         
+
             this.template("_header.html", "app/header.html", context);
         },
 
@@ -34,33 +34,33 @@ var GeniGenerator = yeoman.generators.Base.extend({
 					content: "Demo Section",
 					id: this._.classify("Demo Section")
 				}
-		   
+
 				var fileBase = Date.now() + "_" + this._.underscored("Demo Section");
 				var htmlFile = "app/sections/" + fileBase + ".html";
-				var cssFile  = "app/css/" + fileBase + ".css"; 
-		   
+				var cssFile  = "app/css/" + fileBase + ".css";
+
 				this.template("_section.html", htmlFile, context);
 				this.template("_section.css", cssFile, context);
 			}
 		},
 		generateMenu: function(){
 			var menu = this.read("_menu.html");
-		 
+
 			var t = '<a><%= name %></a>';
 			var files = this.expand("app/sections/*.html");
-		 
+
 			for (var i = 0; i < files.length; i++) {
 				var name = this._.chain(files[i]).strRight("_").strLeftBack(".html").humanize().value();
-		   
+
 				var context = {
 					name: name,
 					id: this._.classify(name)
 				};
-		   
+
 				var link = this.engine(t, context);
 				menu = this.append(menu, "div.menu", link);
 			}
-		 
+
 			this.write("app/menu.html", menu);
 		},
         runNpm: function(){
@@ -74,10 +74,10 @@ var GeniGenerator = yeoman.generators.Base.extend({
   promptUser: function() {
 
         var done = this.async();
- 
+
         // have Yeoman greet the user
         console.log(this.yeoman);
- 
+
         var prompts = [{
             name: 'appName',
             message: 'What is your app\'s name ?'
@@ -95,17 +95,17 @@ var GeniGenerator = yeoman.generators.Base.extend({
             message: 'Would you like to generate a demo section ?',
             default: true
         }];
- 
+
         this.prompt(prompts, function (props) {
             this.appName = props.appName;
-			this.casinoName = props.casinoName;
-			this.addLangSection = props.addLangSection;
+	  this.casinoName = props.casinoName;
+	  this.addLangSection = props.addLangSection;
             this.addDemoSection = props.addDemoSection;
-   
+
             done();
         }.bind(this));
     }
-  
+
 });
 
 module.exports = GeniGenerator;
